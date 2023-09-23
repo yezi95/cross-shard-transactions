a556ef72ef7fa6ddfb84471fe0977bcaedddd54d
yezi95 <75473333+yezi95@users.noreply.github.com>
2023-09-23 14:18:17 +0800

    Update GSSC.sol

--- GSSC.sol
+++ GSSC.sol
@@ -5,20 +5,20 @@ pragma solidity >=0.4.22 <0.9.0;
 contract GSSC{
 /* This creates a contract to obtain the account state*/
     address owner;
-    // mapping the address to account balance
+    // mapping the address to the account balance
     mapping (address => uint256) public balances;
-    // mapping the address to account lockedbalance
+    // mapping the address to account locked balance
     mapping(address => uint) public lockedBalances;
-    // mapping the address to account deposites
+    // mapping the address to account deposits
     mapping(address => uint) public deposits;
-    // mapping the address to shard number
+    // mapping the address to the shard number
     mapping(address => uint) public shardk;
    // the number of votes.
     uint public counter; 
-    //the total number of the arbitors is set as 10, and the threshold is set as 6.
+    //the total number of the arbitrators is set as 10, and the threshold is set as 6.
     uint tarbitor = 6; 
     mapping(address => string ) public signature; 
-    //the deadline time for node function call 
+    //the deadline time for the node function call 
     uint public epochtime; 
     uint public time;
 
